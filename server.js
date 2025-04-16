8046211a4bb06d06b05d7959a1e538df01d0f530
shubhamdix <109404688+shubhamdix@users.noreply.github.com>
2025-04-16 12:48:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+require("dotenv").config(); // load .env file
 
 const app = express();
 app.use(cors());
 
 app.get("/get-location", async (req, res) => {
     try {
-        const response = await axios.get("https://ipinfo.io/json?token=350711f9584ea2");
+        const response = await axios.get(`https://ipinfo.io/json?token=${process.env.IPINFO_TOKEN}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch location" });
     }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+//app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
